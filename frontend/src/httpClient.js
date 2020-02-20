1124368eb78799ddd2043e972206b5f818ffc9c0
Yuxin Bai <ybai@pivotal.io>
2020-02-20 10:54:19 -0500

    Remove session cookies from the backend app
    This is to avoid overriding Gateway session from the backend app.
    
    Signed-off-by: Yuxin Bai <ybai@pivotal.io>

--- frontend/src/httpClient.js
+++ frontend/src/httpClient.js
@@ -4,10 +4,6 @@ export default class HttpClient {
 
     #backendBaseUrl = process.env.REACT_APP_BACKEND_BASE_URL || '';
 
-    constructor() {
-        axios.defaults.withCredentials = true;
-    }
-
     async getAnimals() {
         return axios
             .get(`${this.#backendBaseUrl}/animals`)
@@ -15,49 +11,20 @@ export default class HttpClient {
     }
 
     async submitAdoptionRequest({animalId, email, notes}) {
-        const request = () =>
-            axios.post(`${this.#backendBaseUrl}/animals/${animalId}/adoption-requests`,
-                {email, notes});
-        return this.hackYesGimmeDaCookies()
-            .then(request);
+        return axios.post(`${this.#backendBaseUrl}/animals/${animalId}/adoption-requests`,
+            {email, notes});
     }
 
     async editAdoptionRequest({animalId, adoptionRequestId, email, notes}) {
-        const request = () =>
-            axios.put(`${this.#backendBaseUrl}/animals/${animalId}/adoption-requests/${adoptionRequestId}`,
-                {email, notes});
-        return this.hackYesGimmeDaCookies()
-            .then(request);
+        return axios.put(`${this.#backendBaseUrl}/animals/${animalId}/adoption-requests/${adoptionRequestId}`,
+            {email, notes});
     }
 
     async deleteAdoptionRequest({animalId, adoptionRequestId}) {
-        const request = () =>
-            axios.delete(`${this.#backendBaseUrl}/animals/${animalId}/adoption-requests/${adoptionRequestId}`);
-        return this.hackYesGimmeDaCookies()
-            .then(request);
+        return axios.delete(`${this.#backendBaseUrl}/animals/${animalId}/adoption-requests/${adoptionRequestId}`);
     }
 
     async getUsername() {
-        const testURL = `${this.#backendBaseUrl}/whoami`;
-        const request = () => axios.get(testURL).then(res => res.data);
-        return this.hackYesGimmeDaCookies()
-            .then(response => {
-                if (response.url.endsWith(testURL)) {
-                    return response.text()
-                } else {
-                    return request();
-                }
-            });
-
-    }
-
-    async hackYesGimmeDaCookies() {
-        const testURL = `${this.#backendBaseUrl}/whoami`;
-        const myInit = {
-            method: 'GET',
-            mode: 'no-cors', // TODO: this should go away once CORS issue is solved on gateway, then we can switch back to axios
-            credentials: 'include',
-        };
-        return fetch(new Request(testURL, myInit))
+        return axios.get(`${this.#backendBaseUrl}/whoami`).then(res => res.data);
     }
 }
