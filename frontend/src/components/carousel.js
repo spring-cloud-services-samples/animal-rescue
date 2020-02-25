ea236106462b70cfa094b9f0d6559b1ee4f6fa4d
Erick Arias <searchlightdesigns@gmail.com>
2020-02-25 16:48:59 -0500

    modified the carousel a bit so it wasn't so tall

--- frontend/src/components/carousel.js
+++ frontend/src/components/carousel.js
@@ -2,24 +2,15 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 
 const images = [
-    "http://lorempixel.com/output/cats-q-c-640-480-1.jpg",
-    "https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074_960_720.jpg",
-    "http://lorempixel.com/output/cats-q-c-640-480-3.jpg",
-    "https://cdn.pixabay.com/photo/2016/02/18/18/37/puppy-1207816_960_720.jpg",
-    "http://lorempixel.com/output/cats-q-c-640-480-5.jpg",
-    "https://cdn.pixabay.com/photo/2016/03/09/15/21/dog-1246610_960_720.jpg",
-    "http://lorempixel.com/output/cats-q-c-640-480-7.jpg",
-    "https://cdn.pixabay.com/photo/2019/02/06/15/18/puppy-3979350_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2018/06/03/08/57/cat-3449999_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2018/03/18/18/06/australian-shepherd-3237735_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2016/03/28/10/05/kitten-1285341_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2018/05/07/10/49/husky-3380550_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2016/07/15/15/55/dachshund-1519374_960_720.jpg",
+    "/carousel-1.jpg",
+    "/carousel-2.jpg",
+    "/carousel-3.jpg",
+    "/carousel-4.jpg",
+    "/carousel-5.jpg",
 ];
 
 export default () => (
-  <Carousel autoPlay infiniteLoop>
+  <Carousel infiniteLoop showThumbs={false} showStatus={false} autoPlay showIndicators={false}>
       {
           images.map((image, i) => (
               <div key={i}>
