2ee0cd7cfccfbec6b9d8a95c383e928c2fbc3a51
Yuxin Bai <ybai@pivotal.io>
2020-09-25 17:53:27 -0400

    Use skaffold to deploy the animal-rescue app and SCG object to k8s

---
+++ backend/src/main/java/io/spring/cloud/samples/animalrescue/backend/KubernetesSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package io.spring.cloud.samples.animalrescue.backend;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@Profile("k8s")
+public class KubernetesSecurityConfiguration {
+
+	private static final Logger LOG = LoggerFactory.getLogger(KubernetesSecurityConfiguration.class);
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+		return httpSecurity.httpBasic().disable().build();
+	}
+
+}
