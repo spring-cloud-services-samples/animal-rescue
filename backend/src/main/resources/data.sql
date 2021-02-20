9053b7adb8023dff62a7623d9efd4add220b516e
dekt <dekel@vmware.com>
2021-02-20 13:47:37 -0800

    fix-mypets-image-url

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -2,7 +2,7 @@ insert into animal (id, name, rescue_date, avatar_url, description) values
 (1, 'Castle', '2012-10-02', 'https://www.dropbox.com/s/giczozctzg3tact/castle.jpg?dl=0', 'Castle is the ultimate fluffy companion. You will find it hard to stop petting him.'),
 (2, 'Luke', '2015-07-19', 'https://www.dropbox.com/s/iuuhzkzrjfxtbix/luke.jpg?dl=0', 'Luke will be your number one admirer. You will be forever protected and loved.'),
 (3, 'Sarah', '2012-06-06', 'https://www.dropbox.com/s/70r9fgo5g5ae395/sarah.jpg?dl=0', 'Sarah is the smart (and hungry) one. No fooling her easily.'),
-(4, 'Rey', '2018-05-14', 'https://www.dropbox.com/s/9faopxetp1qjb8g/rey.jpg?dl=0', 'Rey wants to play all day long. You will never get bored.'),
+(4, 'Rey', '2018-05-14', 'https://www.dropbox.com/s/9faopxetp1qjb8g/rey.jpg', 'Rey wants to play all day long. You will never get bored.'),
 (5, 'Tony', '2012-06-06', 'https://www.dropbox.com/s/cao20bah8rswj5u/tony.jpg?dl=0', 'Tony chooses her friends carefully. But once she does, she gives it all.'),
 (6, 'Ginger', '2020-01-12', 'https://cdn.pixabay.com/photo/2015/12/29/20/51/dog-1113398_960_720.jpg', 'Ginger is a posh Pomeranian that loves to be pampered by her humans.'),
 (7, 'Whiskers', '2020-01-20', 'https://cdn.pixabay.com/photo/2017/01/20/21/22/kitten-asleep-in-a-pot-1995961_960_720.jpg', 'Whiskers is a sweet and loving little kitten. She loves cuddling with her humans.'),
@@ -10,15 +10,16 @@ insert into animal (id, name, rescue_date, avatar_url, description) values
 (9, 'Sabrina', '2020-01-24', 'https://cdn.pixabay.com/photo/2016/11/29/01/10/adorable-1866475_960_720.jpg', 'Sabrina is a friendly kitten with beautify blue eyes. She does not need lots of attention, but loves a good belly rub.'),
 (10, 'Georgie', '2020-01-29', 'https://cdn.pixabay.com/photo/2017/03/14/19/06/bulldog-2144110_960_720.jpg', 'Georgie will slobber all over you with his kisses. Does not require a lot of physical activity.');
 
+https://www.dropbox.com/s/6tsua2thxy0ouba/Photo%20Mar%2009%2C%2022%2047%2013.jpg?dl=0
 insert into adoption_request (id, animal, adopter_name, email, notes) values
 (1, 1, 'Bella', 'bella@email.com', 'Blah blah'),
 (2, 1, 'Gareth', 'gareth@email.com', 'Blah blah'),
 (3, 1, 'Chris', 'chris@email.com', 'Blah blah'),
 (4, 2, 'Alberto', 'alberto@email.com', 'Blah blah'),
-(5, 2, 'Roy', 'roy@email.com', 'Blah blah'),
+(5, 1, 'Roy', 'roy@email.com', 'Blah blah'),
 (6, 2, 'Dylan', 'dylan@email.com', 'Blah blah'),
-(7, 2, 'Craig', 'craig@email.com', 'Blah blah'),
+(7, 4, 'Craig', 'craig@email.com', 'Blah blah'),
 (8, 2, 'Alexey', 'alexey@email.com', 'Blah blah'),
-(9, 2, 'Erick', 'erick@email.com', 'Blah blah'),
-(10, 3, 'Ben', 'ben@email.com', 'Blah blah'),
-(11, 3, 'Mike', 'mike@email.com', 'Blah blah');
+(9, 4, 'Erick', 'erick@email.com', 'Blah blah'),
+(10, 5, 'Ben', 'ben@email.com', 'Blah blah'),
+(11, 5, 'Mike', 'mike@email.com', 'Blah blah');
