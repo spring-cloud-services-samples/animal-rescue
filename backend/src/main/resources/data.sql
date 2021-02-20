9d19115edeab1e99e3b7fbcdedb838899c7bee38
dekt <dekel@vmware.com>
2021-02-20 14:09:10 -0800

    remove dl=0 from dropbox images links

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into animal (id, name, rescue_date, avatar_url, description) values
-(1, 'Castle', '2012-10-02', 'https://www.dropbox.com/s/giczozctzg3tact/castle.jpg?dl=0', 'Castle is the ultimate fluffy companion. You will find it hard to stop petting him.'),
-(2, 'Luke', '2015-07-19', 'https://www.dropbox.com/s/iuuhzkzrjfxtbix/luke.jpg?dl=0', 'Luke will be your number one admirer. You will be forever protected and loved.'),
-(3, 'Sarah', '2012-06-06', 'https://www.dropbox.com/s/70r9fgo5g5ae395/sarah.jpg?dl=0', 'Sarah is the smart (and hungry) one. No fooling her easily.'),
+(1, 'Castle', '2012-10-02', 'https://www.dropbox.com/s/giczozctzg3tact/castle.jpg', 'Castle is the ultimate fluffy companion. You will find it hard to stop petting him.'),
+(2, 'Luke', '2015-07-19', 'https://www.dropbox.com/s/iuuhzkzrjfxtbix/luke.jpg', 'Luke will be your number one admirer. You will be forever protected and loved.'),
+(3, 'Sarah', '2012-06-06', 'https://www.dropbox.com/s/70r9fgo5g5ae395/sarah.jpg', 'Sarah is the smart one. No fooling her easily.'),
 (4, 'Rey', '2018-05-14', 'https://www.dropbox.com/s/9faopxetp1qjb8g/rey.jpg', 'Rey wants to play all day long. You will never get bored.'),
-(5, 'Tony', '2012-06-06', 'https://www.dropbox.com/s/cao20bah8rswj5u/tony.jpg?dl=0', 'Tony chooses her friends carefully. But once she does, she gives it all.'),
+(5, 'Tony', '2012-06-06', 'https://www.dropbox.com/s/cao20bah8rswj5u/tony.jpg', 'Tony chooses her friends carefully. But once she does, she gives it all.'),
 (6, 'Ginger', '2020-01-12', 'https://cdn.pixabay.com/photo/2015/12/29/20/51/dog-1113398_960_720.jpg', 'Ginger is a posh Pomeranian that loves to be pampered by her humans.'),
 (7, 'Whiskers', '2020-01-20', 'https://cdn.pixabay.com/photo/2017/01/20/21/22/kitten-asleep-in-a-pot-1995961_960_720.jpg', 'Whiskers is a sweet and loving little kitten. She loves cuddling with her humans.'),
 (8, 'Mittens', '2020-01-22', 'https://cdn.pixabay.com/photo/2013/05/17/15/54/cat-111793_960_720.jpg', 'Mittens is a chubby cat that loves spending his days lounging and eating catnip.'),
