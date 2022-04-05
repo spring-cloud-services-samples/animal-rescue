3bc81c4aa2a04ae38ee20d444a7febd55554f514
maly7 <paul.aly@outlook.com>
2022-04-05 13:19:08 -0400

    regenerate package-lock.json

--- frontend/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: "react-app",
-  rules: {
-    "semi": ["error", "always"],
-    "comma-dangle": ["error", "always-multiline"],
-    "no-unused-vars": ["error"],
-    "no-useless-constructor": ["error"],
-  }
-};
